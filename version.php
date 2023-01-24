5ddf90f5b6f30fb824d854ce6d195fe7d3110837
Julian Tovar <julings12@gmail.com>
2023-01-24 10:58:39 -0500

    INT-18643: 4.0 MP1 Version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022122100;
+$plugin->version   = 2023012400;
 $plugin->requires  = 2022041900; // Requires this Moodle version (3.3.0).
 $plugin->component = 'fileconverter_resque';
 $plugin->release   = '4.0.5';
